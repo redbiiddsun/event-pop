8f66de792c3d41f5967b2ab9bbba9688a1b6d02a
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-04-28 02:19:20 +0700

    ♻️ refactor getDetail endpoint to AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getDetail(): object {
+    return {
+      name: 'Eventpop API',
+      version: '1.0.0',
+      description: 'Eventpop is a web application for organize and reserve a event applications',
+      repository: 'Not available',
+    };
   }
 }
