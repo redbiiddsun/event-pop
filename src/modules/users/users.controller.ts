13346d85eb7d027b420faa75dec6e1f8b30b9395
Phanasorn Srisayam <38393603+redbiiddsun@users.noreply.github.com>
2024-05-13 16:22:41 +0700

    feat: Add profile endpoint with JWT authentication

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, Res, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { TransformInterceptor } from 'src/common/interceptors/response.interceptor';
-
+import { AuthGuard } from '@nestjs/passport';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Request } from 'express';
 @Controller('users')
 // @UseInterceptors(TransformInterceptor)
 export class UsersController {
@@ -17,6 +19,12 @@ export class UsersController {
   @Get()
   findAll() {
     return this.usersService.findAll();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  profile(@Req() req: Request) {
+    return req.user;
   }
 
   @Get(':id')
